400358ed041d442327f703ba0edadc87ce4a92a1
Juan Gmo Zapata <jzapatah@gmail.com>
2022-05-16 18:48:26 -0500

    Css code separated by sections, header and main, started working on js to fetch the data from endpoint

---
+++ src/resources/scripts/request.js
@@ -0,0 +1,9 @@
+const requestHotelApi = async () =>{
+    const response = await fetch("https://6256097e8646add390e01d99.mockapi.io/hotels/reservation/hotels");
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+requestHotelApi();
+export {requestHotelApi}
