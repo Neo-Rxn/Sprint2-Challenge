9033e1993f12faf9ddf0a809d12615631f919d84
Juan Gmo Zapata <jzapatah@gmail.com>
2022-05-18 16:53:01 -0500

    Fixed country filter where country = All countries, didnt show any hotel, make a validation on the length of returned array and fixed the issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-// console.log("Hola Mundo");
 import { showCard } from "./resources/scripts/card.js";
-import {requestHotelApi} from "./resources/scripts/request.js";
+import { requestHotelApi } from "./resources/scripts/request.js";
 
 const hotelData = await requestHotelApi();
 showCard(hotelData);
@@ -11,43 +10,39 @@ const checkOut = document.getElementById("checkOut");
 const price = document.getElementById("price");
 const rooms = document.getElementById("rooms");
 
-checkIn.addEventListener("change", function() {
+checkIn.addEventListener("change", function () {
     let input = this.value;
     let dateEntered = new Date(input);
     console.log(input); //e.g. 2015-11-13
     console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
 });
 
-checkOut.addEventListener("change", function() {
+checkOut.addEventListener("change", function () {
     let input = this.value;
     let dateEntered = new Date(input);
     console.log(input); //e.g. 2015-11-13
     console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
 });
-
 
 // console.log(checkIn.value)
 // console.log(checkOut.value)
-console.log(price.value)
-console.log(rooms.value)
+// console.log(price.value);
+// console.log(rooms.value);
 
+const countryFilter = (changedCountry) => {
+    const filteredCountries = hotelData.filter(
+        (element) => element.country === changedCountry
+    );
+    if (filteredCountries.length != 0) {
+        showCard(filteredCountries);
+    } else {
+        showCard(hotelData);
+    }
+};
 
+country.addEventListener("change", () => countryFilter(country.value));
 
-
-
-const countryFilter = (changedCountry) =>{
-console.log(changedCountry)
-
-const ciudadesFiltradas = hotelData.filter((element) => element.country === changedCountry);
-showCard(ciudadesFiltradas)
-}
-
-
-country.addEventListener("change", ()=> countryFilter(country.value));
-
-const filtros = document.getElementsByClassName("filter")
+const filtros = document.getElementsByClassName("filter");
 // console.log(filtros)
 
 filtros[0].addEventListener("change", () => console.log(filtros[3].value));
-
-
